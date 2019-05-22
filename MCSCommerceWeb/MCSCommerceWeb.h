5991a90eb3e2210185732c4d66a8bd3e185b7d3e
e050246 <bret.deasy@mastercard.com>
2019-05-22 16:36:19 -0400

    Initial Commit

---
+++ MCSCommerceWeb/MCSCommerceWeb.h
@@ -0,0 +1,19 @@
+//
+//  MCSCommerceWeb.h
+//  MCSCommerceWeb
+//
+//  Created by Deasy, Bret on 5/22/19.
+//  Copyright © 2019 Mastercard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCSCommerceWeb.
+FOUNDATION_EXPORT double MCSCommerceWebVersionNumber;
+
+//! Project version string for MCSCommerceWeb.
+FOUNDATION_EXPORT const unsigned char MCSCommerceWebVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSCommerceWeb/PublicHeader.h>
+
+
