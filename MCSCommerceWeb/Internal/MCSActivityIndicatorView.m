d97abe6ad67e615d233a1584549cf77a18647b10
Bret Deasy <bjdeasy@gmail.com>
2019-06-21 17:19:36 -0400

    Remove visual effects code from indicator view

--- MCSCommerceWeb/Internal/MCSActivityIndicatorView.m
+++ MCSCommerceWeb/Internal/MCSActivityIndicatorView.m
@@ -28,24 +28,20 @@
 @implementation MCSActivityIndicatorView
 
 - (instancetype)initWithTitle:(NSString *)title {
-//    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-//    if (self = [super initWithEffect:blurEffect]) {
-//        self.blurEffect = blurEffect;
     if (self = [super init]) {
         self.title = title;
         self.indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-//        self.vibrancyView = [[UIVisualEffectView alloc] initWithEffect:[UIVibrancyEffect effectForBlurEffect:blurEffect]];
         self.label = [[UILabel alloc] init];
         self.cancelButton = [[UIButton alloc] init];
+        self.hidden = YES;
         
         [_label setText:title];
         [_cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
-//        [self.contentView addSubview:_vibrancyView];
-//        [_vibrancyView.contentView addSubview:_indicator];
-//        [_vibrancyView.contentView addSubview:_label];
+        
         [self addSubview:_indicator];
         [self addSubview:_label];
         [self addSubview:_cancelButton];
+        
         [_indicator startAnimating];
     }
     
@@ -58,16 +54,16 @@
     UIView *superview = self.superview;
     CGFloat width = superview.frame.size.width / 2.3;
     CGFloat height = 150.0;
+    CGFloat indicatorSize = 50;
     
     self.frame = CGRectMake(superview.frame.size.width / 2 - width / 2, superview.frame.size.height / 2 - height / 2, width, height);
-//    self.vibrancyView.frame = self.bounds;
     
-    CGFloat indicatorSize = 50;
     self.indicator.frame = CGRectMake(width / 2 - indicatorSize / 2, height / 4 - indicatorSize / 2, indicatorSize, indicatorSize);
     
     self.layer.cornerRadius = 8.0;
     self.layer.masksToBounds = true;
     self.layer.backgroundColor = [[UIColor lightGrayColor] CGColor];
+    self.layer.zPosition = FLT_MAX;
     
     self.label.enabled = YES;
     self.label.textAlignment = NSTextAlignmentCenter;
@@ -85,11 +81,11 @@
 }
 
 - (void)show {
-    self.hidden = false;
+    [self setHidden:NO];
 }
 
 - (void)hide {
-    self.hidden = true;
+    [self setHidden:YES];
 }
 
 
